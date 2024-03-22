9ccb927da54e69f95a821a66eb827a4301697708
Sude Bayram <143509351+ssudebayram@users.noreply.github.com>
2024-03-22 12:03:14 +0300

    Add files via upload

---
+++ Sayımtopla.cs
@@ -0,0 +1,14 @@
+﻿int sayiAdedi;
+int toplam = 0;
+
+Console.WriteLine("kaç sayı gireceksiniz");
+sayiAdedi = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i < sayiAdedi; i++)
+{
+    Console.WriteLine("sayı giriniz");
+    int sayi = Convert.ToInt32(Console.ReadLine());
+
+    toplam += sayi;
+}
+Console.WriteLine("girilen sayıların toplamı " + toplam );
